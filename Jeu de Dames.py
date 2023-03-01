272c9d1a7fe4788744829c19676577f7c89b8352
Rayane-WD <122975227+Rayane-WD@users.noreply.github.com>
2023-03-01 21:17:38 +0100

    Update assets folder

--- Jeu de Dames.py
+++ Jeu de Dames.py
@@ -31,65 +31,65 @@ class MainWindow(QMainWindow):
 
 
       #Action 'Nouvelle partie'
-      self.new_game_action = QAction(QIcon("board-game-go.png"),"Nouvelle partie", self)
+      self.new_game_action = QAction(QIcon("assets/board-game-go.png"),"Nouvelle partie", self)
       self.new_game_action.setShortcut(QKeySequence("Ctrl+T"))
       self.new_game_action.triggered.connect(self.startNewGame)
       self.menu_file.addAction(self.new_game_action)
       self.myToolbar.addAction(self.new_game_action)
 
       #Action 'Charger une partie'
-      self.load_action = QAction(QIcon("blue-folder-network-horizontal-open.png"),"Charger une partie", self)
+      self.load_action = QAction(QIcon("assets/blue-folder-network-horizontal-open.png"),"Charger une partie", self)
       self.load_action.setShortcut(QKeySequence("Ctrl+O"))
       self.load_action.triggered.connect(self.onpenGame)
       self.menu_file.addAction(self.load_action)
       self.myToolbar.addAction(self.load_action)
 
       #Action 'Sauvegarder'
-      self.save_action = QAction(QIcon("disk-return.png"),"Sauvegarder", self)
+      self.save_action = QAction(QIcon("assets/disk-return.png"),"Sauvegarder", self)
       self.save_action.setShortcut(QKeySequence("Ctrl+S"))
       self.save_action.triggered.connect(self.saveGame)
       self.menu_file.addAction(self.save_action)
       self.myToolbar.addAction(self.save_action)
 
       #Action 'avoir les précedents historiques'
-      self.previous_history = QAction(QIcon("a-magnifier-history.png"),"Afficher l'historique des déplacements d'une partie précédente", self)
+      self.previous_history = QAction(QIcon("assets/a-magnifier-history.png"),"Afficher l'historique des déplacements d'une partie précédente", self)
       self.previous_history.setShortcut(QKeySequence("Ctrl+H"))
       self.previous_history.triggered.connect(self.dispPreviousHistory)
       self.menu_history.addAction(self.previous_history)
 
       #Action 'Sauvegarder l'historique des déplacements'
-      self.save_history = QAction(QIcon("a-magnifier--plus.png"),"Sauvegarder l'historique des déplacements", self)
+      self.save_history = QAction(QIcon("assets/a-magnifier--plus.png"),"Sauvegarder l'historique des déplacements", self)
       self.save_history.setShortcut(QKeySequence("Ctrl+P"))
       self.save_history.triggered.connect(self.saveHistory)
       self.menu_history.addAction(self.save_history)
       self.myToolbar.addAction(self.save_history)
 
       #Action 'Manuel'
-      self.help_action = QAction(QIcon("book-question.png"),"Obtenir de l'aide", self)
+      self.help_action = QAction(QIcon("assets/book-question.png"),"Obtenir de l'aide", self)
       self.help_action.setShortcut(QKeySequence("Ctrl+M"))
       self.help_action.triggered.connect(self.dispHelp)
       self.menu_help.addAction(self.help_action)
       self.myToolbar.addAction(self.help_action)
 
       #Action 'À  propos'
-      self.aboutus_action = QAction(QIcon("a-user-silhouette-question.png"),"À propos", self)
+      self.aboutus_action = QAction(QIcon("assets/a-user-silhouette-question.png"),"À propos", self)
       self.aboutus_action.setShortcut(QKeySequence("Ctrl+W"))
       self.aboutus_action.triggered.connect(self.dispAboutUs)
       self.menu_help.addAction(self.aboutus_action)
       self.myToolbar.addAction(self.aboutus_action)
 
       #Action 'Bleu'
-      self.theme_ocean = QAction(QIcon("a-blue-flag.png"),"Thème Bleu Océan", self)
+      self.theme_ocean = QAction(QIcon("assets/a-blue-flag.png"),"Thème Bleu Océan", self)
       self.theme_ocean.triggered.connect(self.changeThemeO)
       self.menu_theme.addAction(self.theme_ocean)
 
       #Action 'Rouge'
-      self.theme_lava = QAction(QIcon("a-red-flag.png"), "Thème Rouge Lave", self)
+      self.theme_lava = QAction(QIcon("assets/a-red-flag.png"), "Thème Rouge Lave", self)
       self.theme_lava.triggered.connect(self.changeThemeL)
       self.menu_theme.addAction(self.theme_lava)
 
       #Action 'Violet'
-      self.theme_candy = QAction(QIcon("a-flag-pink.png"), "Thème Rose Bonbon", self)
+      self.theme_candy = QAction(QIcon("assets/a-flag-pink.png"), "Thème Rose Bonbon", self)
       self.theme_candy.triggered.connect(self.changeThemeC)
       self.menu_theme.addAction(self.theme_candy)
 
